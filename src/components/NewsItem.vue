fa0e880899f7016cd7166c9dbead0cb83924f7c1
Henry Walker <henrywgwalker@gmail.com>
2022-03-14 16:39:25 +0000

    set up app

---
+++ src/components/NewsItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+    <h1>Latest News</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NewsItem",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Axios from 'axios'
 
+Vue.use(Axios);
 Vue.config.productionTip = false
 
 new Vue({
