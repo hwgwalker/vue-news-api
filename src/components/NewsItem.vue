97bc89062941377da49ad4c798e601e0472b1686
Henry Walker <henrywgwalker@gmail.com>
2022-03-14 17:41:30 +0000

    created axios request

--- src/components/NewsItem.vue
+++ src/components/NewsItem.vue
@@ -1,12 +1,36 @@
 <template>
   <div class="hello">
     <h1>Latest News</h1>
+    <div v-for="item of items" :key="item.id">
+      {{ item.title }}
+    </div>
   </div>
 </template>
 
 <script>
+import Vue from "vue";
+import axios from "axios";
+
+Vue.use(axios);
+
 export default {
   name: "NewsItem",
+  data() {
+    return {
+      items: [],
+    };
+  },
+  async created() {
+    try {
+      const response = await axios.get(
+        "https://niftyapi.madeatsilverchip.com/NewsArticles"
+      );
+      console.log(response);
+      this.items = response.data;
+    } catch (e) {
+      console.log(e);
+    }
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Axios from 'axios'
 
-Vue.use(Axios);
 Vue.config.productionTip = false
 
 new Vue({
