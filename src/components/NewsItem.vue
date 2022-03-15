bc9edf5a911af7f2bc1d046dc65230addaa5e239
Henry Walker <henrywgwalker@gmail.com>
2022-03-15 11:31:23 +0000

    fix axios error

--- src/components/NewsItem.vue
+++ src/components/NewsItem.vue
@@ -10,8 +10,9 @@
 <script>
 import Vue from "vue";
 import axios from "axios";
+import VueAxios from "vue-axios";
 
-Vue.use(axios);
+Vue.use(VueAxios, axios);
 
 export default {
   name: "NewsItem",
